5f2bee770b1f0f61f9202d5486baa7f9fd4db478
Bünyamin Enis <bekgithub@gmail.com>
2025-06-22 22:26:56 +0300

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+import random
+
+BALL_COR = [10 ,-10]
+random_xcor = random.choice(BALL_COR)
+random_ycor = random.choice(BALL_COR)
+class Ball(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = random_xcor
+        self.y_move = random_ycor
+        self.ball_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.ball_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.ball_speed = 0.1
+        self.bounce_x()
